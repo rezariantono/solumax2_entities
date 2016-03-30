f94f009d975dd86fc9bb55ffb2c00ac66ea6374e
Reza Riantono <rezariantono@gmail.com>
2016-03-30 13:44:32 +0700

    Initial commit

---
+++ Solumax/Entity/App/Entity/EntityModel.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Solumax\Entity\App\Entity;
+
+
+class EntityModel {
+    //put your code here
+}
---
+++ Solumax/Entity/SolumaxEntityProvider.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Solumax\Entity;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Routing\Router;
+
+class SolumaxEntityProvider extends ServiceProvider {
+    
+    public function boot(Router $router) {
+        
+        require __DIR__ . '/Http/routes.php';
+        
+        $this->loadViewsFrom(__DIR__ . '/Resources/Views', 'entity');
+        
+        $this->publishes([
+            __DIR__.'/Database/Migrations/' => database_path('migrations/entity')
+        ], 'migrations');
+        
+    }
+    
+    public function register() {
+        
+    }
+}
