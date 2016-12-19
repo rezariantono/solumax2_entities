037d8419589290a1ef1613c456a5d2d83abd2e4d
Reza Riantono <rezariantono@gmail.com>
2016-12-19 15:30:40 +0700

    Added jwt key changer

--- Solumax/Entity/Http/Routes/Api.php
+++ Solumax/Entity/Http/Routes/Api.php
@@ -1,6 +1,6 @@
 <?php
 
-$middlewares = ['wala.jwt.header.parser', 'wala.jwt.header.validation',
+$middlewares = ['auth.jwt_key_modifier', 'wala.jwt.header.parser', 'wala.jwt.header.validation',
     'auth.db.groupOverwrite'];
 
 Route::group(['prefix' => 'api', 'namespace' => 'Api', 'middleware' => $middlewares], function() {
