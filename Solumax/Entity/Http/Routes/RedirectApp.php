409e8db28851379b9b8b2cca65bcaf91a4252838
Reza Riantono <rezariantono@gmail.com>
2017-02-11 23:21:57 +0700

    Updated redirect to pass jwt as well

--- Solumax/Entity/Http/Routes/RedirectApp.php
+++ Solumax/Entity/Http/Routes/RedirectApp.php
@@ -7,6 +7,19 @@ Route::group(['prefix' => 'redirect-app'], function() {
     });
      
     Route::get('entity/{id}', function($id) {
-       return redirect('/tenant/index.html#/entity/show/' . $id); 
+        
+        if (request()->has('jwt')) {
+            
+            $params = http_build_query([
+                'state' => '#/entity/show/' . $id,
+                'jwt' => request()->get('jwt')
+            ]);
+            
+            return redirect('/tenant/index.html?' . $params);
+            
+        } else {
+            
+            return redirect('/tenant/index.html#/entity/show/' . $id);
+        } 
     });
 });
